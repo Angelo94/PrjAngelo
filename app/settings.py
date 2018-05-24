c06842ff8721292a4fd438ac97cd69cfa8614fd1
Angelo94 <angelo.calabria94@gmail.com>
2018-05-25 00:36:13 +0200

    Postgres updates

--- app/settings.py
+++ app/settings.py
@@ -72,10 +72,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vehicles',
+        'USER':'angelo',
+        'PASSWORD':'angelo',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
